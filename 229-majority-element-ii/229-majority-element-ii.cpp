f0c91f3a0cf76999c385181fea242a032c71584a
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-06-18 22:04:41 +0530

    Time: 31 ms (10.25%), Space: 15.7 MB (81.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -3,24 +3,46 @@ class Solution
     public:
         vector<int> majorityElement(vector<int> &nums)
         {
-            unordered_map<int, int> mp;
-            int n = nums.size();
-            vector<int> v;
-            for (int i = 0; i < n; i++)
+            int crr1 = -1;
+            int crr2 = -1;
+            int cnt1 = 0;
+            int cnt2 = 0;
+            for (int i: nums)
             {
-                mp[nums[i]]++;
-                if (mp[nums[i]] > n / 3)
+                if (i == crr1)
+                    cnt1++;
+                else if (i == crr2)
+                    cnt2++;
+                else if (cnt1==0)
                 {
-                    if (v.size() == 0)
-                        v.push_back(nums[i]);
-                    else if (v[0] != nums[i])
-                    {
-                        v.push_back(nums[i]);
-                        return v;
-                        break;
-                    }
+                    crr1 = i;
+                    cnt1 = 1;
+                }
+                else if (cnt2==0)
+                {
+                    crr2 = i;
+                    cnt2 = 1;
+                }
+                else
+                {
+                    cnt1--;
+                    cnt2--;
                 }
             }
+            cnt1=cnt2=0;
+            for(auto i:nums)
+            {
+                if(i==crr1)
+                    cnt1++;
+                if(i==crr2)
+                    cnt2++;
+            }
+            vector<int> v;
+            if(cnt1>nums.size()/3)
+                v.push_back(crr1);
+            if(cnt2>nums.size()/3 && crr1!=crr2)
+                v.push_back(crr2);
+            
             return v;
         }
 };
