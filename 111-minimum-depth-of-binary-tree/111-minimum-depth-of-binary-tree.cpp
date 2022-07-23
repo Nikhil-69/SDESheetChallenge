398093345fe78e84fc003423c8511e052abdb05d
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-07-24 00:52:10 +0530

    Time: 388 ms (49.08%), Space: 144.7 MB (43.92%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -16,9 +16,10 @@ public:
             return 0;
         int left=minDepth(root->left);
         int right=minDepth(root->right);
-        return left && right ?min(left,right)+1:max(left,right)+1;
-        
-        
-        
+        if(left==0)
+            return right+1;
+        if(right==0)
+            return left+1;
+        return min(left,right)+1;
     }
 };
