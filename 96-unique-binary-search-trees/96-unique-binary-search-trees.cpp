561a584ba5cca77a0cff248754f92e5115ca498b
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-07-24 00:45:11 +0530

    Time: 4 ms (24.85%), Space: 5.9 MB (65.85%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,16 +1,11 @@
 class Solution {
-public:
-//     unsigned long long int fact(int n){
-        
-//     }
-//     unsigned long long int fact(int n){
-    
+public: 
     int numTrees(int n) {
-        unsigned long long int ans=1;
+        long long ans=1;
         for(int i=n+1;i<=2*n;i++)
         {
             ans*=i;
-            ans/=i-n;
+            ans/=(i-n);
         }
         return ans/(n+1);
     }
