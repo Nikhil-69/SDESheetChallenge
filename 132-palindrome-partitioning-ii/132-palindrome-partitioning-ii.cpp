b4949811d38d5d9495171abfa22a96cb9fd21064
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-07-29 17:05:53 +0530

    Time: 383 ms (58.89%), Space: 37.3 MB (19.68%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,48 +1,42 @@
-int dp[2001][2001];
 class Solution
 {
     public:
-    bool isPalindrome (string & s, int i, int j){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++; j--;
-        }
-        return true;
-    }
-    
 
-    int solve (string & s, int i, int j, vector<vector<int>> & dp){    
-        
-        if(i>=j or isPalindrome(s, i, j)) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
+        bool check_palindrome(string &s, int i, int j)
+        {
+            while (j > i)
+            {
+                if (s[i++] != s[j--])
+                    return false;
+            }
+            return true;
+        }
+    int mincut_helper(string &s, int i, int j,vector<vector<int>> &dp)
+    {
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i >= j || check_palindrome(s, i, j))
+            return 0;
         
         int ans = INT_MAX;
-        
-        for(int k=i; k<j; k++){
+        for (int k = i; k < j; k++)
+        {
             
-            /* 
-                Instead of writing below standard line
-                We will recurse for only right part
-                Only when left part turns out to be palindrome
+            if (check_palindrome(s, i, k))
+            { 
+                if (dp[k + 1][j] == -1)
+                dp[k + 1][j] = mincut_helper(s, k + 1, j,dp);
                 
-                int temp =  solve (s, i, k, dp, palindrome) + solve (s, k+1, j, dp, palindrome) + 1;
-                
-            */
-            
-            if(isPalindrome(s, i, k)){                         
-                int temp = solve (s, k+1, j, dp) + 1;
-                ans = min (ans, temp);
+                int temp = 1 + dp[k + 1][j];
+                ans = min(ans, temp);
             }
         }
-        
         return dp[i][j] = ans;
     }
-    
-    int minCut(string s) {
+    int minCut(string s)
+    {
         int n = s.length();
         vector<vector<int>> dp (n+1, vector<int> (n+1, -1));
-            
-        return solve (s, 0, n-1, dp);
+        return mincut_helper(s, 0, n- 1,dp);
     }
 };
