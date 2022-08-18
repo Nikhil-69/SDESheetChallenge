ce8b7fe6e8625176ef9becd85b4c9da3b9cdae30
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-08-19 02:57:56 +0530

    Time: 87 ms (13.17%), Space: 23.1 MB (6.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,61 +1,49 @@
 
 #define pii pair<int, int>
-    class Solution
+class Solution
+{
+    public:
+    void dfs(vector<vector < char>> &grid, int i, int j)
     {
-        public:
-            void dfs(vector<vector < char>> &grid, int i, int j)
-            {
-                int n = grid.size();
-                int m = grid[0].size();
-                queue<pii> q;
-                q.push({ i,j });
-                while (q.size())
-                {
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    q.pop();
-                    vector<pii> dir = {{ 1,0 },{ -1,0 },{ 0, 1 },{ 0,-1 }};
-                    for (auto it: dir)
-                    {
-                        int newx=x + it.first;
-                        int newy= y + it.second;
-                        if (newx >= 0 && newx < n && newy >= 0 && newy < m && grid[newx][newy] == '1')
-                        {
-                            q.push({ newx,newy});
-                            grid[newx][newy] = '0';
-                        }
-                    }
-                }
-                return;
-            }
-        int numIslands(vector<vector < char>> &grid)
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pii> q;
+        q.push({ i,j });
+        while (q.size())
         {
-            int n = grid.size();
-            int m = grid[0].size();
-            // vector<vector<int>> vis(n, vector<int>(m));
-            // for (int i = 0; i < n; i++)
-            // {
-            //     for (int j = 0; j < m; j++)
-            //     {
-            //         if (grid[i][j] == '0')
-            //         {
-            //             vis[i][j] = 1;
-            //         }
-            //         else vis[i][j] = 0;
-            //     }
-            // }
-            int cnt = 0;
-            for (int i = 0; i < n; i++)
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            vector<pii> dir = {{ 1,0 },{ -1,0 },{ 0, 1 },{ 0,-1 }};
+            for (auto it: dir)
             {
-                for (int j = 0; j < m; j++)
+                int newx=x + it.first;
+                int newy= y + it.second;
+                if (newx >= 0 && newx < n && newy >= 0 && newy < m && grid[newx][newy] == '1')
                 {
-                    if (grid[i][j] == '1')
-                    {
-                        dfs(grid, i, j);
-                        cnt++;
-                    }
+                    q.push({ newx,newy});
+                    grid[newx][newy] = '0';
                 }
             }
-            return cnt;
         }
-    };
+        return;
+    }
+    int numIslands(vector<vector < char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    dfs(grid, i, j);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
