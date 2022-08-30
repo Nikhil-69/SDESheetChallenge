bd5bc39eb2ba762256a0cc32f0eda051e226526b
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2022-08-31 03:58:57 +0530

    Time: 9 ms (60.37%), Space: 19 MB (5.06%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,22 @@
-class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp)
+class Solution
+{
+    public:
+        int minimumTotal(vector<vector < int>> &triangle, int i, int j,
+                         vector< vector< int>> &dp)
+        {
+            if (j >= triangle[i].size())
+                return INT_MAX;
+            if (i == triangle.size() - 1)
+                return triangle[i][j];
+            if (dp[i][j] != -1)
+                return dp[i][j];
+            int down = minimumTotal(triangle, i + 1, j, dp);
+            int diagonal = minimumTotal(triangle, i + 1, j + 1, dp);
+            return dp[i][j] = min(down, diagonal) + triangle[i][j];
+        }
+    int minimumTotal(vector<vector < int>> &triangle)
     {
-        if(j>=triangle[i].size())
-            return INT_MAX;
-        if(i==triangle[triangle.size()-1].size()-1)
-            return triangle[i][j];
-        if(dp[i][j]!=-1)return dp[i][j];
-        int down=minimumTotal(triangle,i+1,j,dp);
-        int downRight=minimumTotal(triangle,i+1,j+1,dp);
-        return dp[i][j]=min(down,downRight)+triangle[i][j];
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(201,vector<int> (201,-1));
-        return minimumTotal(triangle,0,0,dp);
+        vector<vector < int>> dp(201, vector<int> (201, -1));
+        return minimumTotal(triangle, 0, 0, dp);
     }
 };
